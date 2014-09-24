4af78c76da2d7ef2a02cb6c3c7907cb2929ce115
Wesam Mortagi <wesam.mortagi@dashsoft.com>
2014-09-25 00:32:28 +0300

    test reload

--- scripts/controllers/submenusController.js
+++ scripts/controllers/submenusController.js
@@ -1,8 +1,11 @@
 (function () {
     'use strict';
-    angular.module('GMA').controller('SubmenusController', function($scope, api, menuHandler, $stateParams) {
+    angular.module('GMA').controller('SubmenusController', function($scope, api, menuHandler, $stateParams, $state) {
         api.menus.submenus($stateParams.id).then(function(data) {
             $scope.menu = data;
+            
+            $state.go($state.current, {}, { reload: true });
+            
             _.map($scope.menu.Children, function (menuItem) {
                 var templateId = "0";
                 if (menuItem.TemplateId != null)
